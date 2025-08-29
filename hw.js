106b0141e7b71f8a34c9eb20a6cdc99864d436a7
Ayanacyan <amitpacl@gmail.com>
2025-08-28 21:34:37 -0500

    DOM strucure hw

---
+++ hw.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    document.getElementById("result").innerHTML=
+    document.getElementById("demo").firstChild.nodeValue;
+    document.getElementById("result1").innerHTML=
+    document.getElementById("demo").childnodes[0].nodeValue;
+    
+}
